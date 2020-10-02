0f4ebd0f5253d12d96044f53bc2f27834451def7
Muqsit <hackhack05@gmail.com>
2020-10-02 13:13:41 +0000

    4.0: Derive Beacon block properties from PMMP's Beacon

--- src/muqsit/evdbeacon/block/BlockFactory.php
+++ src/muqsit/evdbeacon/block/BlockFactory.php
@@ -5,18 +5,18 @@ declare(strict_types=1);
 namespace muqsit\evdbeacon\block;
 
 use muqsit\evdbeacon\block\tile\Beacon as BeaconTile;
-use pocketmine\block\BlockBreakInfo;
 use pocketmine\block\BlockFactory as VanillaBlockFactory;
 use pocketmine\block\BlockIdentifier;
-use pocketmine\block\BlockLegacyIds;
-use pocketmine\block\BlockToolType;
 use pocketmine\block\tile\TileFactory;
-use pocketmine\item\ItemIds;
+use pocketmine\block\VanillaBlocks;
 
 final class BlockFactory{
 
 	public static function init() : void{
-		VanillaBlockFactory::getInstance()->register(new Beacon(new BlockIdentifier(BlockLegacyIds::BEACON, 0, ItemIds::BEACON, BeaconTile::class), "Beacon", new BlockBreakInfo(0.5, BlockToolType::PICKAXE, 0, 80.0)), true);
+		$parent = VanillaBlocks::BEACON();
+		$parent_id_info = $parent->getIdInfo();
+		VanillaBlockFactory::getInstance()->register(new Beacon(new BlockIdentifier($parent_id_info->getBlockId(), $parent_id_info->getVariant(), $parent_id_info->getItemId(), BeaconTile::class), $parent->getName(), $parent->getBreakInfo()), true);
+
 		TileFactory::getInstance()->register(BeaconTile::class, ["evdbeacon:beacon"]);
 	}
 }
